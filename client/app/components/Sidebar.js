9d917a5f394cff685d2efa37314adb5b2c0bed6b
Eyosiyas Mekbib <eyosi9000@gmail.com>
2024-05-15 22:14:37 +0300

    update(dashboard): remove upgrade to pro card

--- client/app/components/Sidebar.js
+++ client/app/components/Sidebar.js
@@ -73,22 +73,6 @@ const Sidebar = () => {
             </Link>
           </nav>
         </div>
-        <div className='mt-auto p-4'>
-          <Card>
-            <CardHeader className='p-2 pt-0 md:p-4'>
-              <CardTitle>Upgrade to Pro</CardTitle>
-              <CardDescription>
-                Unlock all features and get unlimited access to our support
-                team.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className='p-2 pt-0 md:p-4 md:pt-0'>
-              <Button size='sm' className='w-full'>
-                Upgrade
-              </Button>
-            </CardContent>
-          </Card>
-        </div>
       </div>
     </div>
   );
