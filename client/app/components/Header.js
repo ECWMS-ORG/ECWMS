2cd25604a3ea2726b42900db1258a4178ee58f64
Eyosiyas Mekbib <eyosi9000@gmail.com>
2024-05-15 22:47:23 +0300

    feat(dashboard): remove product search bar from header

--- client/app/components/Header.js
+++ client/app/components/Header.js
@@ -32,7 +32,7 @@ import { Badge } from "@/components/ui/badge";
 
 const Header = () => {
   return (
-    <header className='flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6'>
+    <header className='flex h-14 justify-between items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6'>
       <Sheet>
         <SheetTrigger asChild>
           <Button variant='outline' size='icon' className='shrink-0 md:hidden'>
@@ -88,36 +88,9 @@ const Header = () => {
               Analytics
             </Link>
           </nav>
-          <div className='mt-auto'>
-            <Card>
-              <CardHeader>
-                <CardTitle>Upgrade to Pro</CardTitle>
-                <CardDescription>
-                  Unlock all features and get unlimited access to our support
-                  team.
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <Button size='sm' className='w-full'>
-                  Upgrade
-                </Button>
-              </CardContent>
-            </Card>
-          </div>
         </SheetContent>
       </Sheet>
-      <div className='w-full flex-1'>
-        <form>
-          <div className='relative'>
-            <Search className='absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground' />
-            <Input
-              type='search'
-              placeholder='Search products...'
-              className='w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3'
-            />
-          </div>
-        </form>
-      </div>
+      <div className='w-full flex-1'></div>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant='secondary' size='icon' className='rounded-full'>
@@ -125,7 +98,7 @@ const Header = () => {
             <span className='sr-only'>Toggle user menu</span>
           </Button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent align='end'>
+        <DropdownMenuContent align='start' side='right'>
           <DropdownMenuLabel>My Account</DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuItem>Settings</DropdownMenuItem>
