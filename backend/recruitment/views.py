eff863361ff1274ed9389aa2a8610060f451ddb1
Eyosiyas Mekbib <eyosi9000@gmail.com>
2024-05-29 21:19:43 +0300

    create api views

--- backend/recruitment/views.py
+++ backend/recruitment/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import JobPosition, Candidate, OnBoarding
+from .serializers import JobPositionSerializer, CandidateSerializer, OnBoardingSerializer
 
-# Create your views here.
+class JobPositionViewSet(viewsets.ModelViewSet):
+    queryset = JobPosition.objects.all()
+    serializer_class = JobPositionSerializer
+
+class CandidateViewSet(viewsets.ModelViewSet):
+    queryset = Candidate.objects.all()
+    serializer_class = CandidateSerializer
+
+class OnBoardingViewSet(viewsets.ModelViewSet):
+    queryset = OnBoarding.objects.all()
+    serializer_class = OnBoardingSerializer
